bd60bffda6ba836706d6b4a97000e9645f7b2d8a
Pavel Constantin-Beniamin <constantin.pavel@student.unitbv.ro>
2026-03-11 13:16:20 +0200

    implement state validation and possible moves

--- src/state_logic.pl
+++ src/state_logic.pl
@@ -1,15 +1,20 @@
+initial_state(state(left, left, left, left)).
+final_state(state(right, right, right, right)).
 
-initial_state(state(left,left,left,left)).
-final_state(state(right,right,right,right)).
+# Helper fact for opposite banks
+opposite(left, right).
+opposite(right, left).
 
-#Goat with cabbage without farmer
-danger(state(farmer,wolf,goat,cabbage)) :-
-    farmer \= goat,
-    goat = cabbage.
+# Goat with cabbage without farmer
+danger(state(Farmer, _Wolf, Goat, Cabbage)) :-
+    Farmer \= Goat,
+    Goat = Cabbage.
 
-#Goat with wolf without farmer
-danger(state(farmer,wolf,goat,cabbage)) :-
-    farmer \= goat,
-    goat = wolf.
+# Goat with wolf without farmer
+danger(state(Farmer, Wolf, Goat, _Cabbage)) :-
+    Farmer \= Goat,
+    Goat = Wolf.
 
-valid(state) :-\+ danger(state).
+# State is valid
+valid(State) :- 
+    \+ danger(State).
